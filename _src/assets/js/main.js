6b5a03dc3180d84aa49158b53dad71fa8b55c7f6
Saralopezlovon <lopezlovonsara@gmail.com>
2019-12-02 19:26:07 +0100

    Add function toggel

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,7 +6,22 @@ const containerElement = document.querySelector(".js-container");
 const containerFavElement = document.querySelector(".js-container-fav");
 
 let shows = [];
-const favoritesShows = [];
+let favoritesShows = [];
+
+/*LocalStorage
+function setLocalStorage() {
+  localStorage.setItem("favoritesShows", JSON.stringify(favoritesShows));
+}
+function getLocalStorage() {
+  const localStorageFavoritesShows = JSON.parse(
+    localStorage.getItem("favoritesShows")
+  );
+  if (localStorageFavoritesShows !== null) {
+    favoritesShows = localStorageFavoritesShows;
+    paintShows();
+    listenShows();
+  }
+}*/
 
 const search = function(ev) {
   ev.preventDefault();
@@ -28,20 +43,20 @@ const search = function(ev) {
 function paintFavShows() {
   let htmlCode = `<ul>`;
   for (let i = 0; i < favoritesShows.length; i++) {
-    htmlCode += `<li class="shows__item js-shows-item" id="${favoritesShows[i].show.id}">`;
+    htmlCode += `<li class="shows__item js-shows-item" id="${favoritesShows[i].id}">`;
     /*if (isfavorite === true) {
       htmlCode += `<li class="shows__item js-shows-item shows__item--favorite" id="${i}">`;
     }else{
       htmlCode += `<li class="shows__item js-shows-item" id="${i}">`;
     };*/
 
-    htmlCode += `<h2 class="shows__name">${favoritesShows[i].show.name}</h2>`;
+    htmlCode += `<h2 class="shows__name">${favoritesShows[i].name}</h2>`;
 
-    if (favoritesShows[i].show.image === null) {
+    if (favoritesShows[i].image === null) {
       htmlCode += `<img class="image shows__imagefav" src="https://via.placeholder.com/210x295/ffffff/666666/?
       text=TV.">`;
     } else {
-      htmlCode += `<img class="image shows__imagefav" src="${favoritesShows[i].show.image.medium}">`;
+      htmlCode += `<img class="image shows__imagefav" src="${favoritesShows[i].image.medium}">`;
     }
 
     htmlCode += `</li>`;
@@ -80,18 +95,21 @@ function paintShows() {
 }
 
 function toggleFavorites(ev) {
-  /*console.log("comprobando..");*/
   const clickedId = parseInt(ev.currentTarget.id);
-  for (let i = 0; i < shows.length; i++) {
-    if (clickedId === shows[i].show.id) {
-      favoritesShows.push(shows[i]);
-      paintFavShows();
-    } else {
-      /*paintFavShows();*/
+  const index = favoritesShows.findIndex(function(show, index) {
+    return show.id === clickedId;
+  });
+
+  const isFavorite = index !== -1;
+  if (isFavorite === true) {
+    favoritesShows.splice(index, 1);
+  } else {
+    for (let i = 0; i < shows.length; i++) {
+      if (shows[i].show.id === clickedId) {
+        favoritesShows.push(shows[i].show);
+      }
     }
   }
-
-  console.log(clickedId, favoritesShows);
   paintFavShows();
 }
 
